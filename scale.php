322e106ed54918fa5fdc499845f29db72f164240
IsmaelKleintjes <45165373+IsmaelKleintjes@users.noreply.github.com>
2020-03-09 10:33:09 +0100

    verbetering

--- scale.php
+++ scale.php
@@ -3,10 +3,10 @@ $input = readline();
 $input_values = explode(' ', $input);
 
 $max_weight_value = max($input_values[1], $input_values[0]);
-$min__weight_value = min($input_values[1], $input_values[0]);
-$weight_difference = $max_weight_value - $min__weight_value;
+$min_weight_value = min($input_values[1], $input_values[0]);
+$weight_difference = $max_weight_value - $min_weight_value;
 $balance_weights = explode(',', $input_values[2]);
-
+arsort($balance_weights);
 
 if ($input_values[0] == $input_values[1])
 {
@@ -18,7 +18,8 @@ else
     {
         if ($weight_difference >= $balance_weight)
         {
-            $weight_list[ ] = $balance_weight;
+            if ($balance_weight + $balance_weight)
+            $weight_list[] = $balance_weight;
             $weight_difference -= $balance_weight;
         }
     }
@@ -31,8 +32,6 @@ else
 if (isset($response)) {
     echo $response;
 } else {
-    foreach ($weight_list as $response_weights => $response_weight)
-    {
-        echo $response_weight . ' ';
-    }
+    echo implode(", ", $weight_list) . "\n";
 }
+?>
